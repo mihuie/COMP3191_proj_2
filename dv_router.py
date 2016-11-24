cd9a0a8351c5ad0d3d7ea3a1128874fc9434ec5b
mihuie <huie.michael@gmail.com>
2016-11-23 20:35:13 -0500

    fixed dv router

--- dv_router.py
+++ dv_router.py
@@ -1,16 +1,17 @@
-"""Your awesome Distance Vector router for CS 168."""
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 import sim.api as api
 import sim.basics as basics
-from itertools import product
 
-# We define infinity as a distance of 16.
+# We define infinity as a cost of 16.
 INFINITY = 16
-
+# DEFAULT_TIMER_INTERVAL = 3
 
 class DVRouter(basics.DVRouterBase):
-    # NO_LOG = True # Set to True on an instance to disable its logging
-    # POISON_MODE = True # Can override POISON_MODE here
+
+    # NO_LOG = flood=True # Set to flood=True on an instance to disable its logging
+    POISON_MODE = True  # Can override POISON_MODE here
     # DEFAULT_TIMER_INTERVAL = 5 # Can override this yourself for testing
 
     def __init__(self):
@@ -18,43 +19,45 @@ class DVRouter(basics.DVRouterBase):
         Called when the instance is initialized.
 
         You probably want to do some additional initialization here.
-
         """
-        self.start_timer()      # Starts calling handle_timer() at correct rate
-        self.my_HOST = {}
-        self.route_TABLE = {}   # { host : (cost, port, time) }
-        self.D_VECTORS = {}     # {neigh : {dest:latency}}
-        self.links = {}
-        self.neighbours = {}
 
-        # self.route_TABLE[self] = {self: (0)}
+        self.links = {}
+        self.routing_tb = {}
+        self.d_vectors = {}  
+        self.known_hosts = {}
+
+        self.start_timer()
 
     def handle_link_up(self, port, latency):
         """
         Called by the framework when a link attached to this Entity goes up.
 
-        The port attached to the link and the link latency are passed
-        in.
-
+        The port attached to the link and the link latency are passed in.
         """
+
         self.links[port] = latency
-        # sending routing table
-        for x in self.route_TABLE.keys():
-            cost = self.route_TABLE[x][0]
-            self.send(basics.RoutePacket(x, cost), port, flood=False)
+        self.d_vectors[port] = {}
+        for (host, cost) in self.routing_tb.items():
+            self.send(basics.RoutePacket(host, cost[0]), port, flood=False)
 
     def handle_link_down(self, port):
-        """
+        """#
         Called by the framework when a link attached to this Entity does down.
 
         The port number used by the link is passed in.
-
         """
+
         del self.links[port]
-        del self.neighbours[port]
-        for host in self.route_TABLE.keys():
-            if self.route_TABLE[host][1] == port:
-                pass
+        del self.d_vectors[port]
+        for host in self.known_hosts.keys():
+            if self.known_hosts[host] == port:
+                del self.known_hosts[host]
+
+        for host in self.routing_tb.keys():
+            if self.routing_tb[host][1] == port:
+                new_cost = INFINITY
+                new_port = port
+                self.calc_send(new_cost, new_port, host)
 
     def handle_rx(self, packet, port):
         """
@@ -64,9 +67,8 @@ class DVRouter(basics.DVRouterBase):
         port is the port number it arrived on.
 
         You definitely want to fill this in.
-
         """
-        # self.log("RX %s on %s (%s)", packet, port, api.current_time())
+
         if isinstance(packet, basics.RoutePacket):
             self.handle_RoutePacket(packet, port)
         elif isinstance(packet, basics.HostDiscoveryPacket):
@@ -75,63 +77,105 @@ class DVRouter(basics.DVRouterBase):
             self.handle_DataPacket(packet, port)
 
     def handle_RoutePacket(self, packet, port):
-        """
-        Called by handle_rx to Route packets
+        host = packet.destination
+        self.d_vectors[port][host] = packet.latency
+        cost = min(self.links[port] + packet.latency, INFINITY)
 
-        """
-        time = api.current_time()
-        if packet.src not in self.neighbours.keys():
-            self.neighbours[packet.src] = port
+        if host in self.routing_tb.keys():
+            if self.routing_tb[host][1] == port:
+                current_best = self.links[port] + packet.latency
+                current_port = port
+                self.calc_send(current_best, current_port, host)
 
-        if packet.src not in self.D_VECTORS.keys():
-            self.D_VECTORS[packet.src] = {}
+        elif host not in self.routing_tb or cost < self.routing_tb[host][0]:
 
-        self.D_VECTORS[packet.src].update({packet.destination: packet.latency})
-
-        if packet.destination not in self.route_TABLE.keys():
-            self.route_TABLE[packet.destination] = (INFINITY, port, time)
-
-        for x in self.D_VECTORS.keys():
-            cost = self.links[port] + self.D_VECTORS[packet.src][packet.destination]
-            if cost < self.route_TABLE[packet.destination][0]:
-                self.route_TABLE[packet.destination] = (cost, port, time)
+            self.routing_tb[host] = [cost, port, api.current_time()]
+            if cost == INFINITY:
+                if self.POISON_MODE:
+                    self.send(basics.RoutePacket(host, cost), flood=True)
+            else:
+                self.send(basics.RoutePacket(host, cost), port, flood=True)        
 
     def handle_HostDiscoveryPacket(self, packet, port):
-        """
-        Called by handle_rx to Host discovery pacts packets
-
-        """
-        if packet.src is not None and packet.src not in self.my_HOST.keys():
-            self.my_HOST[packet.src] = port
+        self.routing_tb[packet.src] = [self.links[port], port, 
+                api.current_time()]
+        self.known_hosts[packet.src] = port
+        self.send(basics.RoutePacket(packet.src,
+                  self.routing_tb[packet.src][0]), port, flood=True)
 
     def handle_DataPacket(self, packet, port):
-        """
-        Called by handle_rx to other packets
-
-        """
-        if packet.dst in self.my_HOST.keys():
-            self.send(packet, self.my_HOST[packet.dst], flood=False)
-        elif packet.dst in self.route_TABLE.keys():
-            out_port = self.route_TABLE[packet.dst][1]
-            self.send(packet, out_port, flood=False)
-        else:
-            self.send(packet, port, flood=True)
+        if packet.dst in self.routing_tb.keys():
+            if self.routing_tb[packet.dst][0] < INFINITY \
+                and self.routing_tb[packet.dst][1] != port:
+                self.send(packet,
+                          self.routing_tb[packet.dst][1], flood=False)
 
     def handle_timer(self):
         """
         Called periodically.
 
-        When called, your router should send tables to neighbors.  It
-        also might not be a bad place to check for whether any entries
-        have expired.
-
+        When called, your router should send tables to neighbours.  It also might
+        not be a bad place to check for whether any entries have expired.
         """
-        for x in self.route_TABLE.keys():
-            cost, port, time = self.route_TABLE[x]
-            if api.current_time() - time >= 15:
-                self.route_TABLE[x] = (INFINITY, port, time)
 
-        for x, y in product(self.neighbours.keys(), self.route_TABLE.keys()):
-            port = self.neighbours[x]
-            cost = self.route_TABLE[y][0]
-            self.send(basics.RoutePacket(x, cost), port, flood=False)
+        time = api.current_time()
+
+        for host in self.routing_tb.keys():
+            if host not in self.known_hosts:
+                time = time - self.routing_tb[host][2]
+                temp_cost = self.routing_tb[host][0]
+                temp_port = self.routing_tb[host][1]
+
+                if time >= 15 or temp_cost >= INFINITY:
+
+                    if temp_port in self.d_vectors:
+                        del self.d_vectors[temp_port][host]
+
+                    cost = INFINITY
+                    port = self.routing_tb[host][1]
+
+                    cost, port, time = self.b_ford(cost, port, host)
+                    self.routing_tb[host] = [cost, port, time]
+
+                    if cost == INFINITY:
+                        del self.routing_tb[host]
+                        if self.POISON_MODE:
+                            self.send(basics.RoutePacket(host, INFINITY), flood=True)
+                    else:
+                        self.send(basics.RoutePacket(host, cost), port, flood=True)
+
+        for (host, values) in self.routing_tb.items():
+            cost = values[0]
+            port = values[1]
+            if cost == INFINITY:
+                if self.POISON_MODE:
+                    self.send(basics.RoutePacket(host, cost), flood=True)
+            else:
+                self.send(basics.RoutePacket(host, cost), port, flood=True)
+
+    def b_ford(self, cost, port, host):
+        # bell ford
+        for neighbour in self.d_vectors.keys():
+            if host in self.d_vectors[neighbour]:
+                if self.links[neighbour] + self.d_vectors[neighbour][host] < cost:
+                    cost = self.links[neighbour] + self.d_vectors[neighbour][host]
+                    port = neighbour
+        return cost, port, api.current_time()
+        
+    def calc_send(self, cost, port, host):
+        
+        self.routing_tb[host] = self.b_ford(cost, port, host)
+
+        if cost == INFINITY:
+            if self.POISON_MODE:
+                self.routing_tb[host] = [INFINITY, port, api.current_time()]
+                self.send(basics.RoutePacket(host, cost), port, flood=True)
+            elif host in self.routing_tb:
+                del self.routing_tb[host]
+        else:
+            self.routing_tb[host] = [cost, port, api.current_time()]
+            self.send(basics.RoutePacket(host, cost), port, flood=True)
+
+
+
+            
